76bb5461fa60708cd98e3a43e6cf52108a1f2318
PucTv <128126363+ThaiVanPhuc@users.noreply.github.com>
2024-10-29 16:40:31 +0700

    update

---
+++ KTHP_Web1/src/test/java/com/example/KTHP_Web1/KthpWeb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KTHP_Web1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KthpWeb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
